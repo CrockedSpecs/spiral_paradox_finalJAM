c45f94d4aa564919eec9c206c2afdc30615fbc97
Wbeimar Agudelo <andrey.agudelo12@gmail.com>
2024-12-08 16:08:24 -0500

    ObjectPool
    People in Main Menu
    Experience

--- Assets/Scripts/Level/SpawnExperience.cs
+++ Assets/Scripts/Level/SpawnExperience.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class PeopleFalling : MonoBehaviour
+public class SpawnExperience : MonoBehaviour
 {
     //Declarations
-    [SerializeField] private List<GameObject> people;
+    [SerializeField] private GameObject experience;
 
-    [SerializeField] private List<GameObject> pooledPeople;
+    [SerializeField] private List<GameObject> pooledExperience;
 
     // Start is called before the first frame update
     void Start()
     {
         InstancePool(10);
-        TurnOffPooledPeople();
-
-        InvokeRepeating("SpawnPeople", 2, 2);
+        TurnOffPooledExperience();
     }
 
     // Update is called once per frame
@@ -32,60 +30,45 @@ public class PeopleFalling : MonoBehaviour
         for (int i = 0; i < amount; i++)
         {
             //creamos un GameObject temporal que almacene la instancia (clon) del prefab
-            GameObject spawnObj = Instantiate(people[SelectPeople()], transform.position, transform.rotation);
+            GameObject spawnObj = Instantiate(experience, transform.position, transform.rotation);
             //añadimos dicho clon a la lista
-            pooledPeople.Add(spawnObj);
+            pooledExperience.Add(spawnObj);
             spawnObj.transform.SetParent(this.transform);
         }
     }
 
-    void TurnOffPooledPeople()
+    void TurnOffPooledExperience()
     {
-        foreach (var clon in pooledPeople)
+        foreach (var clon in pooledExperience)
         {
             //para cada clon dentro de la lista, su estado pasara a inactivo (se apagará) y se volverá hijo del GameObject que contenga este script
             clon.SetActive(false);
         }
     }
 
-    private void SpawnPeople()
+    public void ActivateExperience(Vector3 position, Quaternion rotation)
     {
         GameObject ObjectToActivate = GetOffObject();
-        ObjectToActivate.transform.position = RandomPosition();
-        ObjectToActivate.transform.rotation = RandomRotation();
+        ObjectToActivate.transform.position = position;
+        ObjectToActivate.transform.rotation = rotation;
         GetOffObject().SetActive(true);
     }
 
     private GameObject GetOffObject()
     {
-        for (int i = 0; i < pooledPeople.Count; i++)
+        for (int i = 0; i < pooledExperience.Count; i++)
         {
             //usamos una condición donde preguntamos si el clon actual NO está activado
-            if (!pooledPeople[i].activeInHierarchy)
+            if (!pooledExperience[i].activeInHierarchy)
             {
                 //si NO está activado, lo va a dar como resultado (lo retorna)
-                return pooledPeople[i];
+                return pooledExperience[i];
             }
         }
 
         //aquí se instancia nuevamente 1 elemento adicional (para no causar errores)
         //y retorna ese nuevo objeto sabiendo que es el último en la lista.
         InstancePool(1);
-        return pooledPeople.Last<GameObject>();
-    }
-
-    private int SelectPeople()
-    {
-        return Random.Range(0, people.Count);
-    }
-
-    private Vector3 RandomPosition()
-    {
-        return new Vector3(Random.Range(-70, 71), Random.Range(20, 41), Random.Range(60, 80));
-    }
-
-    private Quaternion RandomRotation()
-    {
-        return new Quaternion(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360), 0);
+        return pooledExperience.Last<GameObject>();
     }
 }
